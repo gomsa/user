a80bb55df1d37a10d48ea63797b73f98dabce07c
bigrocs <bigrocs@qq.com>
2019-06-04 17:03:13 +0800

    自动提交 git 代码

--- middleware/hander_test.go
+++ middleware/hander_test.go
@@ -9,49 +9,16 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	p := []Permission{
-		{
-			Service:     `Users`,
-			Method:      `Create`,
-			Auth:        false,
-			Policy:      false,
-			Name:        `创建用户`,
-			Description: `创建新用户权限。`,
-		},
-		{
-			Service:     `Users`,
-			Method:      `Exist`,
-			Auth:        false,
-			Policy:      false,
-			Name:        `检测用户`,
-			Description: `检测用户是否存在权限。`,
-		},
-		{
-			Service:     `Users`,
-			Method:      `Get`,
-			Auth:        true,
-			Policy:      false,
-			Name:        `用户查询`,
-			Description: `查询用户信息权限。`,
-		},
-		{
-			Service:     `Auth`,
-			Method:      `Auth`,
-			Auth:        false,
-			Policy:      false,
-			Name:        `用户授权`,
-			Description: `用户登录授权返回 token 权限。`,
-		},
-		{
-			Service:     `Auth`,
-			Method:      `ValidateToken`,
-			Auth:        false,
-			Policy:      false,
-			Name:        `权限认证`,
-			Description: `权限相关认证权限。`,
-		},
+	Pata := []map[string]interface{}{
+		{"service": "Users", "method": "Create", "auth": false, "policy": false, "display_name": "创建用户", "description": "创建新用户权限。"},
+		{"service": "Users", "method": "Exist", "auth": false, "policy": false, "display_name": "检测用户", "description": "检测用户是否存在权限。"},
+		{"service": "Users", "method": "Get", "auth": true, "policy": false, "display_name": "用户查询", "description": "查询用户信息权限。"},
+		{"service": "Auth", "method": "Auth", "auth": false, "policy": false, "display_name": "用户授权", "description": "用户登录授权返回 token 权限。"},
+		{"service": "Auth", "method": "ValidateToken", "auth": false, "policy": false, "display_name": "权限认证", "description": "权限相关认证权限。"},
 	}
-	h := Handler{p}
+	h := Handler{Permission{
+		Data: Pata,
+	}}
 	hf := h.Wrapper(HandlerFunc)
 	err := hf(context.Background(), nil, nil)
 	fmt.Println(err)
