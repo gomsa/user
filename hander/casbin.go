5ce0b02e4f264d20cb991b88a88ae617d1fc4a0f
bigrocs <bigrocs@qq.com>
2019-06-12 15:38:26 +0800

    自动提交 git 代码

---
+++ hander/casbin.go
@@ -0,0 +1,37 @@
+package hander
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/casbin/casbin"
+	pb "github.com/gomsa/user-srv/proto/casbin"
+)
+
+// Casbin 权限管理
+type Casbin struct {
+	Enforcer *casbin.Enforcer
+}
+
+// AddPermissions 添加权限
+func (srv *Casbin) AddPermissions(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	for _, permission := range req.Permissions {
+		res.Valid = srv.Enforcer.AddPermissionForUser(req.User, permission)
+		if !res.Valid {
+			return fmt.Errorf("添加权限失败")
+		}
+	}
+	return err
+}
+
+// DeletePermissions 根据角色名删除权限
+func (srv *Casbin) DeletePermissions(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Valid = srv.Enforcer.DeletePermissionsForUser(req.User)
+	return err
+}
+
+// GetPermissions 获取权限
+func (srv *Casbin) GetPermissions(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Permissions = srv.Enforcer.DeletePermissionsForUser(req.User)
+	return err
+}
