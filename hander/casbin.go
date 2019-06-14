287b4e0a266e86694b38746f4ca591044967eb49
bigrocs <bigrocs@qq.com>
2019-06-14 16:52:57 +0800

    自动提交 git 代码

--- hander/casbin.go
+++ hander/casbin.go
@@ -51,3 +51,38 @@ func (srv *Casbin) GetPermissions(ctx context.Context, req *pb.Request, res *pb.
 	}
 	return err
 }
+
+////////////
+
+// AddRole 添加权限
+func (srv *Casbin) AddRole(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Valid = srv.Enforcer.AddRoleForUser(req.UserID, req.Role)
+	if !res.Valid {
+		return fmt.Errorf("添加角色失败")
+	}
+	return err
+}
+
+// DeleteRoles 根据角色名删除权限
+func (srv *Casbin) DeleteRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Valid = srv.Enforcer.DeleteRolesForUser(req.UserID)
+	return err
+}
+
+// UpdateRoles 重新设置角色所有权限
+func (srv *Casbin) UpdateRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Valid = srv.Enforcer.DeleteRolesForUser(req.UserID)
+	for _, role := range req.Roles {
+		res.Valid = srv.Enforcer.AddRoleForUser(req.UserID, role)
+		if !res.Valid {
+			return fmt.Errorf("添加角色失败")
+		}
+	}
+	return err
+}
+
+// GetRoles 获取权限
+func (srv *Casbin) GetRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	res.Roles = srv.Enforcer.GetRolesForUser(req.UserID)
+	return err
+}
