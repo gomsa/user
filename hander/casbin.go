ec677cb10d828b5d6464fe89a19de151030dbea6
bigrocs <bigrocs@qq.com>
2019-07-12 17:43:46 +0800

    自动提交 git 代码

--- hander/casbin.go
+++ hander/casbin.go
@@ -58,7 +58,7 @@ func (srv *Casbin) GetPermissions(ctx context.Context, req *pb.Request, res *pb.
 
 // AddRole 添加权限
 func (srv *Casbin) AddRole(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	res.Valid = srv.Enforcer.AddRoleForUser(req.UserID, req.Role)
+	res.Valid = srv.Enforcer.AddRoleForUser(req.Group, req.Role)
 	if !res.Valid {
 		return errors.New("添加角色失败")
 	}
@@ -67,15 +67,15 @@ func (srv *Casbin) AddRole(ctx context.Context, req *pb.Request, res *pb.Respons
 
 // DeleteRoles 根据角色名删除权限
 func (srv *Casbin) DeleteRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	res.Valid = srv.Enforcer.DeleteRolesForUser(req.UserID)
+	res.Valid = srv.Enforcer.DeleteRolesForUser(req.Group)
 	return err
 }
 
-// UpdateRoles 重新设置角色所有权限
+// UpdateRoles 重新设置用户所有权限
 func (srv *Casbin) UpdateRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	res.Valid = srv.Enforcer.DeleteRolesForUser(req.UserID)
+	res.Valid = srv.Enforcer.DeleteRolesForUser(req.Group)
 	for _, role := range req.Roles {
-		res.Valid = srv.Enforcer.AddRoleForUser(req.UserID, role)
+		res.Valid = srv.Enforcer.AddRoleForUser(req.Group, role)
 		if !res.Valid {
 			return errors.New("添加角色失败")
 		}
@@ -85,7 +85,7 @@ func (srv *Casbin) UpdateRoles(ctx context.Context, req *pb.Request, res *pb.Res
 
 // GetRoles 获取权限
 func (srv *Casbin) GetRoles(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	res.Roles, err = srv.Enforcer.GetRolesForUser(req.UserID)
+	res.Roles, err = srv.Enforcer.GetRolesForUser(req.Group)
 	return err
 }
 
