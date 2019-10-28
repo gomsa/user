e5800ae592133bd00a663c1f99d689b0b8af6a93
bigrocs <bigrocs@qq.com>
2019-10-28 13:38:42 +0800

    自动提交 git 代码

--- client/user.go
+++ client/user.go
@@ -15,11 +15,11 @@ type User struct {
 
 // SyncPermission 同步权限
 func (srv *User) SyncPermission(permissions []*PB.Permission) error {
+	var req PB.Request
 	for _, p := range permissions {
-		req := &PB.Request{
-			Permission: p,
-		}
-		err := client.Call(context.TODO(), srv.ServiceName, "Permissions.UpdateOrCreate", req, nil)
+		req.Permission = p
+		res := &PB.Response{}
+		err := client.Call(context.TODO(), srv.ServiceName, "Permissions.UpdateOrCreate", &req, res)
 		if err != nil {
 			return err
 		}
